705ab795c0d0b7a3af86282e4b5f13999f633f84
IIROFESSOR-X <IIROFESSOR@gmail.com>
2023-10-27 23:20:31 +0300

    mark-2

--- task-1.js
+++ task-1.js
@@ -1,18 +1,11 @@
-
-
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  // const quantity = quantity;
-  // const pricePerDroid = pricePerDroid;
-  
   const totalPrice = quantity * pricePerDroid;
- if (customerCredits<totalPrice) {
-  return "Insufficient funds!"
+  if (customerCredits < totalPrice) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
-
- if (customerCredits>totalPrice || customerCredits===totalPrice ) {return `You ordered ${quantity} droids worth ${totalPrice} credits!`}
- 
-}
-
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
